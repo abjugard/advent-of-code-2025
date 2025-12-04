89554c77172f4ccc3334d3229ee93917785d8db7
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2025-12-04 09:27:11 +0100

    Day 3: Better naming

--- src/day03-lobby.py
+++ src/day03-lobby.py
@@ -3,12 +3,12 @@ from santas_little_helpers import day, get_data, timed
 today = day(2025, 3)
 
 
-def maximise_joltage(joltage_banks, width=2):
+def maximise_joltage(joltage_banks, batt_size=2):
   total_joltage = 0
   for bank in joltage_banks:
     start, joltage = 0, ''
-    for cursor_idx in range(width):
-      end = len(bank) - (width - cursor_idx)
+    for cursor_idx in range(batt_size):
+      end = len(bank) - (batt_size - cursor_idx)
       joltage += max(bank[start:end+1])
       start = bank.index(joltage[-1], start, end+1) + 1
     total_joltage += int(joltage)
